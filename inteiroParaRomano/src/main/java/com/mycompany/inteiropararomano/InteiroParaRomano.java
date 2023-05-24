1d7d6500e593964606323a3f4b3f32d59ce484be
ignizxl <igor.joao@academico.ifpb.edu.br>
2023-05-24 11:26:09 -0300

    update

--- inteiroParaRomano/src/main/java/com/mycompany/inteiropararomano/InteiroParaRomano.java
+++ inteiroParaRomano/src/main/java/com/mycompany/inteiropararomano/InteiroParaRomano.java
@@ -22,6 +22,7 @@ public class InteiroParaRomano {
         // ix = 9
         // xl = 40
         // cd = 400
+        // cm = 900
         String simbolos[] = {"M","CM", "D", "CD", "C", "XC", "L", "XL" , "X", "IX", "V", "IV", "I"};
         int valores[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         
@@ -29,15 +30,17 @@ public class InteiroParaRomano {
         String nRomano= "";
         //pede um entrada de dado, converte pra int e armazena em nInteiro
         int nInteiro = ler.nextInt();
-        
+        //crio um laço for pra iniciar varrendo do maior para o menor elemento
         for (int i = 0; i < 13; i++){
+            //enquanto a minha entrada de dados for maior ou igual ao valor na posição [i], faça
             while (nInteiro >= valores[i]){
+                //adiciono na variável nromano o simbolo na posição i (que é o simbolo da vez)
                 nRomano = nRomano + simbolos[i];
-                nInteiro = nInteiro - valores[i];
-            
-            }
-            
+                //e por fim eu decremento a minha entrada de dados pelo valor da vez que é valor na posição i
+                nInteiro = nInteiro - valores[i];           
+            } 
         }
+        // e por fim, printo na tela o nRomano que é o resultado final
         System.out.println(nRomano);
     }
 }
